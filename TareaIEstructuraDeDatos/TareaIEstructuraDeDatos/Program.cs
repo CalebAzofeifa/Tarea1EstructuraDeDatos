9e2fd7a85d200ec0c256dcd0b1da8790d9b566a0
caleb leon azofeifa <caleb.lazofeifa2707@gmail.com>
2023-05-22 13:46:50 -0600

    Tarea 1 del curso de estructura

---
+++ TareaIEstructuraDeDatos/TareaIEstructuraDeDatos/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class TareaEstructura
+{
+    static void Main(string[] args)
+    {
+        int[] PrimerArreglo = new int[3];
+        int[] SegundoArreglo = new int[3];
+        Console.WriteLine("Ingrese los números del primer arreglo");
+        for (int i = 0; i < 3; i++)
+        {
+            Console.Write("Número {0}: ", i + 1);
+            PrimerArreglo[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        // Llenar el segundo arreglo
+        Console.WriteLine("\nIngrese los números para el segundo arreglo:");
+
+        for (int i = 0; i < 3; i++)
+        {
+            Console.Write("Número {0}: ", i + 1);
+            SegundoArreglo[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        int[] ArregloFinal = new int[6];
+        PrimerArreglo.CopyTo(ArregloFinal, 0);
+        SegundoArreglo.CopyTo(ArregloFinal, PrimerArreglo.Length);
+        Array.Sort(ArregloFinal);
+        Console.WriteLine("El arreglo final da como resultado: ");
+        ImprimirResultado(ArregloFinal);
+        Console.ReadLine();
+
+
+    }
+    static void ImprimirResultado(int[] Arreglo)
+    {
+        foreach (int numero in Arreglo) 
+        {
+            Console.Write("{0} ", numero);
+        }
+    }
+  }           
